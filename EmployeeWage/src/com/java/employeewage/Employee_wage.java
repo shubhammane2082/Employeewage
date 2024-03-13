48658e7ac458334a87e4b8fd13ccedb2539fc077
shubhammane2082 <shubhammane2082@gmail.com>
2024-03-13 11:40:09 +0530

    UC12_EmployeeWage_Multiple_Company

--- EmployeeWage/src/com/java/employeewage/Employee_wage.java
+++ EmployeeWage/src/com/java/employeewage/Employee_wage.java
@@ -8,7 +8,7 @@ public class Employee_wage
 		CalculateEmployeeWage calWage=new  CalculateEmployeeWage();
 		calWage.addemployeeWage("TCS", 20, 20, 120);
 		calWage.addemployeeWage("Wipro", 30, 20, 130);
-		calWage.addemployeeWage("L&T", 35, 20, 120);
+		calWage.addemployeeWage("L&T", 25, 22, 140);
 		calWage.calculateEmployeewage();
 		
 	}
